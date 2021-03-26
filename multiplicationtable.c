e2b8a7f2a4ac3e2067b54278749a408d6d97abc8
mridul-singh3 <81376704+mridul-singh3@users.noreply.github.com>
2021-03-26 18:55:44 +0530

    Add files via upload
    Know the value of any number's multiplication upto 10! just change the value of a variable!

---
+++ multiplicationtable.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+ int main(){
+     // code//
+     int a,b,c,d,e,f,g,h,i,j,k;
+     a=9;
+     b=1;
+     c=2;
+     d=3;
+     e=4;
+     f=5;
+     g=6;
+     h=7;
+     i=8;
+     j=9;
+     k=10;
+     printf("9×1 = %d\n", a*b);
+     printf ("9×2 = %d\n", a*c );
+     printf("9×3 = %d\n", a*d);
+     printf("9×4 = %d\n", a*e);
+     printf("9×5 = %d\n", a*f);
+     printf("9×6 = %d\n", a*g);
+     printf("9×7 = %d\n",a*h);
+     printf("9×8 = %d\n", a*i);
+     printf("9×9 = %d\n", a*j);
+     printf("9×10 = %d\n", a*k);
+     return 0;
+ }
